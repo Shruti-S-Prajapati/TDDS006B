264d71f1a7708f96bd61b34c539f3be5c4e56378
Shruti-S-Prajapati <shruti.s.prajapati2@gmail.com>
2026-06-29 22:57:37 +0530

    fundanetals of building apis

---
+++ http method/post_get.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+class Student(BaseModel):
+     name: str
+     age: int
+students = []
+
+@app.post("/students")
+def create_student(student: Student):
+     students.append(student.dict())
+     return{
+          "message":"Student Created",
+          "student": student
+     }   
+@app.get("/student_data")
+def student():
+     return students
