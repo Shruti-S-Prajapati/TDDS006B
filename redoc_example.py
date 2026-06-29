1f602b47addc9984eae74dfc801130e1d8af2acc
Shruti-S-Prajapati <shruti.s.prajapati2@gmail.com>
2026-06-29 22:32:58 +0530

    First swagger ui and redoc

---
+++ redoc_example.py
@@ -0,0 +1,45 @@
+from fastapi import FastAPI
+
+app = FastAPI(
+    title="My first redoc FastAPI",
+    description="this is simple FastAPI for learning purpose",
+    version='1.0.0'
+)
+
+@app.get("/")
+def home():
+    return {
+        "message": "This is my FastAPI app"
+    }
+
+@app.get("/view")
+def view():
+
+    students = [
+        {
+            "id": 1,
+            "name": "Shruti",
+            "age": 19,
+            "course": "Data Science"
+        },
+        {
+            "id": 2,
+            "name": "Nancy",
+            "age": 20,
+            "course": "Computer Science"
+        },
+        {
+            "id": 3,
+            "name": "Yukti",
+            "age": 18,
+            "course": "Artificial Intelligence"
+        },
+        {
+            "id": 4,
+            "name":"Vishakha",
+            "age": 21,
+            "course":"Machine Learning"
+        }
+    ]
+
+    return students
